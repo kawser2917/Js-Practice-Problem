ee116b496297e789bb4c86c6793cd7591152185d
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-09-16 23:11:40 +0600

    apple orange simple program done

---
+++ apple-orange.js
@@ -0,0 +1,10 @@
+var moneyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var totalCost = applePrice + orangePrice;
+
+console.log(totalCost)
+
+var getBack = moneyGiven - totalCost;
+console.log(getBack)
