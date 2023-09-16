f24933efa3fdececa193942446e84938925526f1
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-09-16 23:32:07 +0600

    array update complete

---
+++ array-update.js
@@ -0,0 +1,7 @@
+var fruits = ["Apple", "Banana", "Orange"];
+var bananaIndex = fruits.indexOf('Banana')
+fruits[bananaIndex] = "Mango"
+console.log(fruits)
+fruits.pop()
+fruits.push("Watermelon")
+console.log(fruits)
