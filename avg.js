0e343f1223cecc10e02358d2afd1a3f24e12cace
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-09-16 23:16:34 +0600

    avg problem solved

---
+++ avg.js
@@ -0,0 +1,9 @@
+var mathmetics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMark =  mathmetics + biology + chemistry + physics + bangla;
+var avg = totalMark/5;
+console.log(avg)
